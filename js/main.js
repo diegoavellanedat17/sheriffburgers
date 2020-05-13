e78d4323a1e860782e1114be1a1dac0fbd6783c9
Diego Avellaneda <avellanedad@javeriana.edu.co>
2020-05-13 10:29:08 -0500

    working with out new json

--- js/main.js
+++ js/main.js
@@ -13,22 +13,80 @@ console.log(today_hora)
 
 const tomorrow= new Date(today)
 tomorrow.setDate(tomorrow.getDate()+1)
-console.log(tomorrow)
+
 
 
 tomorrow_y = tomorrow.getFullYear();
 tomorrow_m = tomorrow.getMonth() + 1;
 tomorrow_d = tomorrow.getDate();
 tomorrow_day_week=tomorrow.getDay()-1
-tomorrow_hora= tomorrow.getHours()
-console.log(tomorrow_hora)
 
-if(today_hora > 10){
+var numero_almuerzos=1;
+
+if(today_hora >= 10){
+    // si es menor que las 10 AM se toma el set del dia de hoy hasta la hora deseada
+
+    var countDownDate = new Date()
+    countDownDate.setDate(countDownDate.getDate()+1)
+    countDownDate.setHours(10)
+    countDownDate.setMinutes(0)
+    countDownDate.setSeconds(0)
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+      
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+      
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML =  hours + " horas "
+    + minutes + " minutos " + seconds + " segundos ";
+      
+
+}, 1000);
+//acaba
+
     $("#cuenta-atras").append(`Puedes hacer tu pedido para mañana hasta las 10 AM`)
     document.getElementById("dia").innerHTML = dias[tomorrow_day_week] +" " + tomorrow_d + " de " + meses[tomorrow_m-1] + " de " + tomorrow_y;
 }
 
 else{
+
+    var countDownDate = new Date()
+    countDownDate.setHours(10)
+    countDownDate.setMinutes(0)
+    countDownDate.setSeconds(0)
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+      
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+      
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML =  hours + " horas "
+    + minutes + " minutos " + seconds + " segundos ";
+      
+}, 1000);
+
     $("#cuenta-atras").append(`Aun puedes hacer tu pedido para hoy hasta las 10 AM`)
     document.getElementById("dia").innerHTML = dias[today_day_week] +" " + today_d + " de " + meses[today_m-1] + " de " + today_y;
 }
@@ -40,22 +98,26 @@ else{
 $(function() {
     $("#addMore").click(function(e) {
       e.preventDefault();
+      numero_almuerzos++;
       $("#almuerzos").append(`<div id="almuerzos">
       <div class="form-group">
-        <label for="exampleFormControlSelect1">Opción de Almuerzo</label>
-        <select class="form-control" id="exampleFormControlSelect1">
+        <label for="opcion${numero_almuerzos}">Opción de Almuerzo ${numero_almuerzos}</label>
+        <select class="form-control" id="opcion${numero_almuerzos}" name="opcion${numero_almuerzos}">
           <option>1</option>
           <option>2</option>
         </select>
       </div>
 
       <div class="form-group">
-        <label for="exampleFormControlSelect1">Tamaño</label>
-        <select class="form-control" id="exampleFormControlSelect1">
+        <label for="tamano${numero_almuerzos}">Tamaño Almuerzo ${numero_almuerzos}</label>
+        <select class="form-control" id="tamano${numero_almuerzos}" name="tamano${numero_almuerzos}">
           <option>32 OZ</option>
           <option>24 OZ</option>
         </select>
       </div>`);
+      console.log(numero_almuerzos)
 
     });
   });
+
+
