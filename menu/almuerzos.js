2c7079db9f5b843178f20e65313bdfee866744f0
Diego Avellaneda <avellanedad@javeriana.edu.co>
2020-05-13 18:01:21 -0500

    Adicionar Menú

--- menu/almuerzos.js
+++ menu/almuerzos.js
@@ -1,65 +1,128 @@
 // http://www.objgen.com/json?demo=true
 // // Empezamos a definir el json de almuerzos
 // menu
-//   13-05-2020
-//     opcion1
-//       titulo= Pechuga a la plancha
-//       ingredientes []= pechuga,arroz,maiz,queso
-//     opcion2
-//       titulo= Carne a la plancha
-//       ingredientes []= pechuga,arroz,maiz,queso
+//    d13052020
+//      opcion1
+//        titulo= Pollo en salsa de champiñon
+//        ingredientes []= pechuga,arroz,maiz,queso
+//      opcion2
+//        titulo= Carne a la plancha
+//        ingredientes []= Pollo en salsa de champiñon,Maíz,Aguacate,Papas Cabello de Ángel,Arroz Blanco
 
-//   14-05-2020
-//     opcion1
-//       titulo= Pechuga a la pizaiola
-//       ingredientes []= pechuga,arroz,maiz,queso
-//     opcion2
-//       titulo= Carne a la pizaiola
-//       ingredientes []= pechuga,arroz,maiz,queso
+//    d14052020
+//      opcion1
+//        titulo= Pollo en salsa de champiñon
+//        ingredientes []= Pollo en salsa de champiñon,Maíz,Aguacate,Papas Cabello de Ángel,Arroz Blanco
+//      opcion2
+//        titulo= Carne de res a la parrilla
+//        ingredientes []= Carne de Res,Maíz,Aguacate,Papas Cabello de Ángel,Arroz Blanco
+
+//   d15052020
+//      opcion1
+//        titulo= Pollo en salsa Miel Mostaza
+//        ingredientes []= Pollo en salsa Miel Mostaza,Monedas de plátano,Arvejas,Zanahoria,Arroz con Coca Cola
+//      opcion2
+//        titulo= Pierna de Cerdo
+//        ingredientes []= Pierna de Cerdo BBQ,Monedas de plátano,Arvejas,Zanahoria,Arroz con Coca Cola
+//   d16052020
+//      opcion1
+//        titulo= Albondigas en salsa Campesina
+//        ingredientes []= Albondigas en salsa Campesina, Pepino,Zucchini,Tomates Cherry,Arroz con cebolla caramelizada,Papas francesa
+//      opcion2
+//        titulo= Pollo apanado
+//        ingredientes []= Pollo apanado,Mayonesa de Aguacate, Pepino,Zucchini,Tomates Cherry,Arroz con cebolla caramelizada,Papas francesa
 
 var menu= `{
-    "menu": {
-      "d13052020": {
-        "opcion1": {
-          "titulo": "Pechuga a la plancha",
-          "ingredientes": [
-            "pechuga",
-            "arroz",
-            "maiz",
-            "queso"
-          ]
-        },
-        "opcion2": {
-          "titulo": "Carne a la plancha",
-          "ingredientes": [
-            "pechuga",
-            "arroz",
-            "maiz",
-            "queso"
-          ]
-        }
+  "menu": {
+    "d13052020": {
+      "opcion1": {
+        "titulo": "Pollo en salsa de champiñon",
+        "ingredientes": [
+          "pechuga",
+          "arroz",
+          "maiz",
+          "queso"
+        ]
       },
-      "d14052020": {
-        "opcion1": {
-          "titulo": "Pollo en salsa de champiñon",
-          "ingredientes": [
-            "Pollo en salsa de champiñon",
-            "Maíz",
-            "Aguacate",
-            "Papas Cabello de ángel",
-            "Arroz blanco"
-          ]
-        },
-        "opcion2": {
-          "titulo": "Carne de Res a la parrilla",
-          "ingredientes": [
-            "Carne de Res",
-            "Maíz",
-            "Aguacate",
-            "Papas Cabello de ángel",
-            "Arroz blanco"
-          ]
-        }
+      "opcion2": {
+        "titulo": "Carne a la plancha",
+        "ingredientes": [
+          "Pollo en salsa de champiñon",
+          "Maíz",
+          "Aguacate",
+          "Papas Cabello de Ángel",
+          "Arroz Blanco"
+        ]
+      }
+    },
+    "d14052020": {
+      "opcion1": {
+        "titulo": "Pollo en salsa de champiñon",
+        "ingredientes": [
+          "Pollo en salsa de champiñon",
+          "Maíz",
+          "Aguacate",
+          "Papas Cabello de Ángel",
+          "Arroz Blanco"
+        ]
+      },
+      "opcion2": {
+        "titulo": "Carne de res a la parrilla",
+        "ingredientes": [
+          "Carne de Res",
+          "Maíz",
+          "Aguacate",
+          "Papas Cabello de Ángel",
+          "Arroz Blanco"
+        ]
+      }
+    },
+    "d15052020": {
+      "opcion1": {
+        "titulo": "Pollo en salsa Miel Mostaza",
+        "ingredientes": [
+          "Pollo en salsa Miel Mostaza",
+          "Monedas de plátano",
+          "Arvejas",
+          "Zanahoria",
+          "Arroz con Coca Cola"
+        ]
+      },
+      "opcion2": {
+        "titulo": "Pierna de Cerdo",
+        "ingredientes": [
+          "Pierna de Cerdo BBQ",
+          "Monedas de plátano",
+          "Arvejas",
+          "Zanahoria",
+          "Arroz con Coca Cola"
+        ]
+      }
+    },
+    "d16052020": {
+      "opcion1": {
+        "titulo": "Albondigas en salsa Campesina",
+        "ingredientes": [
+          "Albondigas en salsa Campesina",
+          "Pepino",
+          "Zucchini",
+          "Tomates Cherry",
+          "Arroz con cebolla caramelizada",
+          "Papas francesa"
+        ]
+      },
+      "opcion2": {
+        "titulo": "Pollo apanado",
+        "ingredientes": [
+          "Pollo apanado",
+          "Mayonesa de Aguacate",
+          "Pepino",
+          "Zucchini",
+          "Tomates Cherry",
+          "Arroz con cebolla caramelizada",
+          "Papas francesa"
+        ]
       }
     }
-  }`;
+  }
+}`;
